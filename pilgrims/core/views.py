47c4bc6a7fb4fcf94e2fd26696c2af58d95490d4
w8s <todd.waits@gmail.com>
2015-05-16 23:13:02 -0400

    initial look and feel for home page

--- pilgrims/core/views.py
+++ pilgrims/core/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, render_to_response
+from django.core.urlresolvers import reverse
+
+
+def home(request):
+    return render(request, 'home.html')
--- pilgrims/pilgrims/urls.py
+++ pilgrims/pilgrims/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'pilgrims.views.home', name='home'),
+    url(r'^$', 'core.views.home', name='home'),
     # url(r'^pilgrims/', include('pilgrims.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
